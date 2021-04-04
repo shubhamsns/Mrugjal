3f0e019ab3b322bc49c8d274a2aaf4dee91ca5a7
Shubham Sananse <shubhamsananse9@gmail.com>
2021-04-04 19:58:15 +0530

    fixed image bug in suggested profile

--- src/components/sidebar/suggested-profile.js
+++ src/components/sidebar/suggested-profile.js
@@ -43,15 +43,36 @@ function SuggestedProfile({ suggestedUser, currentUser, secondary }) {
     <div className="flex flex-row items-center justify-between">
       <div className="flex items-center justify-between">
         <Link to={`/u/${suggestedUser.username}`}>
-          <CloudinaryImage
-            src={suggestedUser.photoURL}
-            alt={`${suggestedUser.username} profile`}
-            size={secondary ? '48' : '42'}
-            type="profile"
-            className={`rounded-full ${
-              secondary ? 'h-12 w-12' : 'h-10 w-10'
-            } flex mr-3.5`}
-          />
+          {suggestedUser.photoURL ? (
+            <CloudinaryImage
+              src={suggestedUser.photoURL}
+              alt={`${suggestedUser.username} profile`}
+              size={secondary ? '48' : '42'}
+              type="profile"
+              className={`rounded-full ${
+                secondary ? 'h-12 w-12' : 'h-10 w-10'
+              } flex mr-3.5`}
+            />
+          ) : (
+            <div>
+              <svg
+                className={`${
+                  secondary ? 'h-12 w-12' : 'h-10 w-10'
+                } text-black-light cursor-pointer mr-3.5 active:text-gray-500`}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </div>
+          )}
         </Link>
         <div>
           <Link
