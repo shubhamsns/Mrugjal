d3bc518b1169fd714063d4110185ecf5d213e3f6
Shubham Sananse <shubhamsananse9@gmail.com>
2021-05-22 22:51:58 +0530

    removed unused vars

--- src/pages/unauthenticated-app/login.js
+++ src/pages/unauthenticated-app/login.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { useMutation } from 'react-query';
 import { useForm } from 'react-hook-form';
